e10406dc7d427d94513f42496f85c415885deeb3
Filipe Silva <filipeqs@outlook.com>
2021-11-28 10:17:30 -0500

    create exercise dto and validator

---
+++ Application/Dtos/Exercise/CreateExerciseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Dtos.Exercise
+{
+    public class CreateExerciseDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ Application/Dtos/Exercise/Validators/CreateExerciseDtoValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Application.Dtos.Exercise.Validators
+{
+    public class CreateExerciseDtoValidator : AbstractValidator<CreateExerciseDto>
+    {
+        public CreateExerciseDtoValidator()
+        {
+            RuleFor(x => x.Name).NotNull().WithMessage("{PropertyName} must be present.");
+        }
+    }
+}
