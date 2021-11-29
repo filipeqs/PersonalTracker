166f246bda28f34381e5608f42ad076357aef9b9
Filipe Silva <filipeqs@outlook.com>
2021-11-29 18:01:11 -0500

    loading

--- client/src/app/exercises/exercise-details/exercise-details.component.ts
+++ client/src/app/exercises/exercise-details/exercise-details.component.ts
@@ -10,13 +10,16 @@ import { ExercisesService } from '../exercises.service';
 })
 export class ExerciseDetailsComponent implements OnInit {
     exercise: ExerciseDetails;
+    loading = false;
 
     constructor(private route: ActivatedRoute, private exerciseService: ExercisesService) {}
 
     ngOnInit(): void {
+        this.loading = true;
         this.route.paramMap.subscribe((params) => {
             const id = +params.get('id');
             this.exerciseService.getExerciseDetails(id).subscribe((exercise) => {
+                this.loading = false;
                 this.exercise = exercise;
             });
         });
