2478d6d8e5c88ad48ae59ed814481e36ecf5d055
Filipe Silva <filipeqs@outlook.com>
2021-12-02 16:35:25 -0500

    add exercise form

--- client/src/app/exercises/exercise-add/exercise-add.component.ts
+++ client/src/app/exercises/exercise-add/exercise-add.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ExerciseCreate } from 'src/app/shared/models/exercises';
 
 @Component({
-  selector: 'app-exercise-add',
-  templateUrl: './exercise-add.component.html',
-  styleUrls: ['./exercise-add.component.scss']
+    selector: 'app-exercise-add',
+    templateUrl: './exercise-add.component.html',
+    styleUrls: ['./exercise-add.component.scss'],
 })
 export class ExerciseAddComponent implements OnInit {
+    exerciseForm = new FormGroup({
+        name: new FormControl('', [Validators.required]),
+        description: new FormControl(''),
+    });
 
-  constructor() { }
+    get name() {
+        return this.exerciseForm.get('name');
+    }
 
-  ngOnInit(): void {
-  }
+    constructor() {}
 
+    ngOnInit(): void {}
+
+    onCreateExercise() {
+        console.log(this.exerciseForm.value);
+    }
 }
--- client/src/app/exercises/exercises.module.ts
+++ client/src/app/exercises/exercises.module.ts
@@ -5,9 +5,10 @@ import { ExercisesRoutingModule } from './exercises-routing.module';
 import { ExercisesComponent } from './exercises.component';
 import { ExerciseDetailsComponent } from './exercise-details/exercise-details.component';
 import { ExerciseAddComponent } from './exercise-add/exercise-add.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [ExercisesComponent, ExerciseDetailsComponent, ExerciseAddComponent],
-    imports: [CommonModule, ExercisesRoutingModule],
+    imports: [CommonModule, ReactiveFormsModule, ExercisesRoutingModule],
 })
 export class ExercisesModule {}
