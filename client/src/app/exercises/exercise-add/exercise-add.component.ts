3604b30ecbf7514df8fc05b57d6185ca66cd7a2d
Filipe Silva <filipeqs@outlook.com>
2021-12-02 16:46:04 -0500

    create exercise

--- client/src/app/exercises/exercise-add/exercise-add.component.ts
+++ client/src/app/exercises/exercise-add/exercise-add.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ExerciseCreate } from 'src/app/shared/models/exercises';
+import { ExercisesService } from '../exercises.service';
 
 @Component({
     selector: 'app-exercise-add',
@@ -12,16 +14,34 @@ export class ExerciseAddComponent implements OnInit {
         name: new FormControl('', [Validators.required]),
         description: new FormControl(''),
     });
+    loading = false;
 
     get name() {
         return this.exerciseForm.get('name');
     }
 
-    constructor() {}
+    get description() {
+        return this.exerciseForm.get('description');
+    }
+
+    constructor(private exercisesService: ExercisesService, private router: Router) {}
 
     ngOnInit(): void {}
 
     onCreateExercise() {
-        console.log(this.exerciseForm.value);
+        const exercise: ExerciseCreate = {
+            name: this.name.value,
+            description: this.description.value,
+        };
+
+        this.exercisesService.createExercise(exercise).subscribe({
+            next: () => {
+                this.loading = false;
+                this.router.navigateByUrl('/exercises');
+            },
+            error: (errors) => {
+                console.log(errors);
+            },
+        });
     }
 }
