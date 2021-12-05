a069621046fc4840c33f91a105fb900a9354adf0
Filipe Silva <filipeqs@outlook.com>
2021-12-04 20:44:11 -0500

    exercise form component

--- client/src/app/exercises/exercise-add/exercise-add.component.ts
+++ client/src/app/exercises/exercise-add/exercise-add.component.ts
@@ -1,8 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { ExerciseCreate } from 'src/app/shared/models/exercises';
-import { ExercisesService } from '../exercises.service';
 
 @Component({
     selector: 'app-exercise-add',
@@ -10,38 +6,7 @@ import { ExercisesService } from '../exercises.service';
     styleUrls: ['./exercise-add.component.scss'],
 })
 export class ExerciseAddComponent implements OnInit {
-    exerciseForm = new FormGroup({
-        name: new FormControl('', [Validators.required]),
-        description: new FormControl(''),
-    });
-    loading = false;
-
-    get name() {
-        return this.exerciseForm.get('name');
-    }
-
-    get description() {
-        return this.exerciseForm.get('description');
-    }
-
-    constructor(private exercisesService: ExercisesService, private router: Router) {}
+    constructor() {}
 
     ngOnInit(): void {}
-
-    onCreateExercise() {
-        const exercise: ExerciseCreate = {
-            name: this.name.value,
-            description: this.description.value,
-        };
-
-        this.exercisesService.createExercise(exercise).subscribe({
-            next: () => {
-                this.loading = false;
-                this.router.navigateByUrl('/exercises');
-            },
-            error: (errors) => {
-                console.log(errors);
-            },
-        });
-    }
 }
--- client/src/app/exercises/exercises.module.ts
+++ client/src/app/exercises/exercises.module.ts
@@ -6,9 +6,10 @@ import { ExercisesComponent } from './exercises.component';
 import { ExerciseDetailsComponent } from './exercise-details/exercise-details.component';
 import { ExerciseAddComponent } from './exercise-add/exercise-add.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ExerciseFormComponent } from './exercise-form/exercise-form.component';
 
 @NgModule({
-    declarations: [ExercisesComponent, ExerciseDetailsComponent, ExerciseAddComponent],
+    declarations: [ExercisesComponent, ExerciseDetailsComponent, ExerciseAddComponent, ExerciseFormComponent],
     imports: [CommonModule, ReactiveFormsModule, ExercisesRoutingModule],
 })
 export class ExercisesModule {}
